94ce94fd05246e2debd6cc717011b7bfdf6f97b4
zahidbiscj <zahidbiscj@gmail.com>
2019-10-03 20:15:22 +0600

    Added ManagingLibraryController.cs

---
+++ LibraryManagementWithWebAPI/Controllers/ManagingLibraryController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibraryManagementWithWebAPI.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagementWithWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ManagingLibraryController : ControllerBase
+    {
+        private IManageLibraryServices _ManageLibraryServices;
+        public ManagingLibraryController(IManageLibraryServices ManageLibraryServices)
+        {
+            _ManageLibraryServices = ManageLibraryServices;
+        }
+
+        [HttpPost("/api/ManagingLibrary/IssueBook")]
+        public void IssueBook([FromBody]string[] values)/// StudentID,Barcode 
+        {
+            _ManageLibraryServices.IssueBook(int.Parse(values[0]), values[1]);
+
+        }
+
+        [HttpPost("/api/ManagingLibrary/ReturnBook")]
+        public void ReturnBook([FromBody] string[] values)/// StudentID,Barcode 
+        {
+            _ManageLibraryServices.ReturnBook(int.Parse(values[0]), values[1]);
+        }
+
+
+    }
+}
---
+++ LibraryManagementWithWebAPI/Controllers/ReportingController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibraryManagementWithWebAPI.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagementWithWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportingController : ControllerBase
+    {
+        private IReportingService _reportingService;
+        public ReportingController(IReportingService reportingService)
+        {
+            _reportingService = reportingService;
+        }
+
+        [HttpPost]
+        public ActionResult<double> CheckFineForEachBook([FromBody] string[] values)//Id,Barcode
+        {
+            return _reportingService.CheckLateFee(int.Parse(values[0]),values[1]);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<double> StudentCheckFine(int Id)
+        {
+            return _reportingService.CheckFine(Id);
+        }
+
+
+    }
+}
